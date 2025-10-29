887f9fd49ef0ae23fff56e767853084c042afbfc
jonathan-ornellas <jonathan.nnt@hotmail.com>
2025-10-29 15:25:48 -0300

    swagger para prod

--- Fiap.Game.Infra.Data/Repository/UserRepository.cs
+++ Fiap.Game.Infra.Data/Repository/UserRepository.cs
@@ -7,18 +7,18 @@ namespace Fiap.Game.Infra.Data.Repository
 {
     public class UserRepository(AppDbContext db) : Repository<User>(db), IUserRepository
     {
+        private readonly AppDbContext _appDbContext = db;
 
         public async Task<User?> GetByEmailAsync(Email email, CancellationToken ct = default)
         {
-            return await db.Users
+            return await _appDbContext.Users
                 .FirstOrDefaultAsync(u => u.Email == email, ct);
         }
 
         public async Task<bool> EmailExistsAsync(Email email, CancellationToken ct = default)
         {
-            return await db.Users
+            return await _appDbContext.Users
                 .AnyAsync(u => u.Email == email, ct);
         }
-
     }
 }
